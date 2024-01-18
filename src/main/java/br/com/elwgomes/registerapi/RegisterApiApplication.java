f2d6b94f9c9fe43e1eed7f96ca2379afe1ed0bb5
Leonardo Gomes <elwgomes@icloud.com>
2024-01-17 21:24:37 -0300

    initial commit

---
+++ src/main/java/br/com/elwgomes/registerapi/RegisterApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.elwgomes.registerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterApiApplication.class, args);
+	}
+
+}
